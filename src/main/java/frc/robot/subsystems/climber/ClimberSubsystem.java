9fcde895abcad90e8c71a43946e6b02a3d9a8c82
Ava Rizzico <avarizzico@gmail.com>
2024-03-22 15:16:24 -0400

    updated climbing

--- src/main/java/frc/robot/subsystems/climber/ClimberSubsystem.java
+++ src/main/java/frc/robot/subsystems/climber/ClimberSubsystem.java
@@ -5,6 +5,7 @@ import com.revrobotics.CANSparkMax;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class ClimberSubsystem extends SubsystemBase {
   private final CANSparkMax leftSpark =
@@ -13,20 +14,28 @@ public class ClimberSubsystem extends SubsystemBase {
   private final CANSparkMax rightSpark =
       new CANSparkMax(Constants.ClimberConstants.climberRightSpark, MotorType.kBrushless);
 
+      
+
   public void startClimb() {
     // spark.setVoltage(SmartDashboard.getNumber("Spin voltage", 0));
     rightSpark.set(Constants.ClimberConstants.climberSpeed);
     leftSpark.set(Constants.ClimberConstants.reverseClimberSpeed);
+    SmartDashboard.putNumber("left climb distance", leftSpark.getEncoder().getPosition());
+    SmartDashboard.putNumber("right climb distance", rightSpark.getEncoder().getPosition());
   }
 
   public void reverseClimb() {
     rightSpark.set(Constants.ClimberConstants.reverseClimberSpeed);
     leftSpark.set(Constants.ClimberConstants.climberSpeed);
+    SmartDashboard.putNumber("left climb distance", leftSpark.getEncoder().getPosition());
+    SmartDashboard.putNumber("right climb distance", rightSpark.getEncoder().getPosition());
   }
 
   public void stopClimb() {
     leftSpark.set(0);
     rightSpark.set(0);
+    SmartDashboard.putNumber("left climb distance", leftSpark.getEncoder().getPosition());
+    SmartDashboard.putNumber("right climb distance", rightSpark.getEncoder().getPosition());
   }
 
   public Command climbCommand() {
