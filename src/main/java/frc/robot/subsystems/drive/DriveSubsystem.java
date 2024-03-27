c40ffbffb96d9d950ec3629669cc55021b4599b2
Ava Rizzico <avarizzico@gmail.com>
2024-03-27 15:38:44 -0400

    reduce drive velocity for kiddos to 25%

--- src/main/java/frc/robot/subsystems/drive/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/drive/DriveSubsystem.java
@@ -276,13 +276,13 @@ public class DriveSubsystem extends SubsystemBase implements AutoCloseable {
     return run(() -> {
           xSpeed.mut_setMagnitude(
               MathUtil.applyDeadband(x.getAsDouble(), 0.01)
-                  * DriveConstants.maxSpeed.in(MetersPerSecond));
+                  * DriveConstants.maxSpeed.in(MetersPerSecond)*.25);
           ySpeed.mut_setMagnitude(
               MathUtil.applyDeadband(y.getAsDouble(), 0.01)
-                  * DriveConstants.maxSpeed.in(MetersPerSecond));
+                  * DriveConstants.maxSpeed.in(MetersPerSecond)*.25);
           omegaSpeed.mut_setMagnitude(
               MathUtil.applyDeadband(-omega.getAsDouble(), .08)
-                  * DriveConstants.maxAngularSpeed.in(RadiansPerSecond));
+                  * DriveConstants.maxAngularSpeed.in(RadiansPerSecond)*.25);
 
           drive(xSpeed, ySpeed, omegaSpeed, ReferenceFrame.FIELD);
         })
